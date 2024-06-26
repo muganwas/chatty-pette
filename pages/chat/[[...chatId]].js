69bcf2388fd3b559efab695ac4a02a39593f7659
Muganwa <stevenmuganwa@live.com>
2024-06-27 00:01:37 +0300

    auth0 setup

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+export default function Chat() {
+    return <div>
+        <Head>
+            <title>Welcome to the ChatGPT starter</title>
+        </Head>
+        <h1>Welcome to the chat id page</h1>
+    </div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div />;
+  return <div>
+    <Head>
+      <title>Welcome to the ChatGPT starter</title>
+    </Head>
+    <h1>Welcome to NextJs & ChatGPT starter</h1>
+    <Link href="/api/auth/login">Login</Link>
+  </div>;
 }
