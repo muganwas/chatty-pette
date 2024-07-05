e6f3230dba148cdfffabbdc137ca83fc60a4fe27
Muganwa <stevenmuganwa@live.com>
2024-07-05 17:15:41 +0300

    style sidebar

--- components/ChatSidebar/ChatSidebar.js
+++ components/ChatSidebar/ChatSidebar.js
@@ -1,3 +1,5 @@
+import { faMessage, faPlus, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -14,10 +16,20 @@ export const ChatSidebar = () => {
         loadChatList();
     }, [])
     return <div className="flex flex-col overflow-hidden bg-gray-900 text-white">
-        <Link className="side-menu-item" href="/chat">New Chat</Link>
-        <div className="flex flex-1 overflow-auto bg-gray-950">
-            {!!chatList && chatList.map(chat => <Link className="side-menu-item" key={chat._id} href={`/chat/${chat.id}`}>{chat.title}</Link>)}
+        <Link className="side-menu-item bg-emerald-500 hover:bg-emerald-600" href="/chat">
+            <FontAwesomeIcon icon={faPlus} />
+            <span>New Chat</span></Link>
+        <div className="flex-1 overflow-auto bg-gray-950">
+            {!!chatList && chatList.map(chat =>
+                <Link className="side-menu-item" key={chat._id} href={`/chat/${chat._id}`}>
+                    <FontAwesomeIcon icon={faMessage} />
+                    <span>{chat.title}</span>
+                </Link>
+            )}
         </div>
-        <Link className="side-menu-item" href="/api/auth/logout">Logout</Link>
+        <Link className="side-menu-item" href="/api/auth/logout">
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <span>Logout</span>
+        </Link>
     </div>
 }
